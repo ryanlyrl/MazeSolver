55d80d2f06cf861ce258e82e4712a741dc8c53cb
Ryan Ly <thinkbeam@gmail.com>
2017-03-22 16:55:35 -0400

    Added Solver

--- src/Maze.java
+++ src/Maze.java
@@ -32,6 +32,10 @@ public class Maze {
 
     }
 
+    public Maze(){
+
+    }
+
     public void assignBlock(){
         for(int i = 0;i < ySize;i++) {
             if (blockedX[0][i]) {
@@ -70,45 +74,54 @@ public class Maze {
                 }
             }
         }
+
+        for(int i = 0;i < xSize;i++){
+            points[i][0].canMoveN = false;
+            points[i][ySize-1].canMoveS = false;
+        }
+
+        for(int i = 0; i < ySize;i++){
+            points[0][i].canMoveW = false;
+            points[xSize - 1][i].canMoveE = false;
+        }
     }
 
     public void solve(){
-        Solver solver = new Solver();
+        Solver solver = new Solver(this);
         solver.run();
     }
 
     @Override
     public String toString(){
-        for(int i = 0;i < ySize * 2 - 1;i++){
-            if(i % 2 == 0){
-                for(int j = 0;j < xSize - 1;j++){
-                    System.out.print("O");
-
-                    if(blockedX[i/2][j]){
-                        System.out.print("--X--");
-                    } else {
-                        System.out.print("-----");
-                    }
-                }
-                System.out.println("O");
-            } else {
-                for(int k = 0;k < 3;k++) {
-                    for (int j = 0; j < xSize - 1; j++) {
-                        if(k == 1) {
-                            if(blockedY[i/2][j]) {
-                                System.out.print("X     ");
-                            } else {
-                                System.out.print("|     ");
-                            }
-                        } else {
-                            System.out.print("|     ");
-                        }
-                    }
-                    System.out.println("|");
-                }
-            }
-        }
-
+//        for(int i = 0;i < ySize * 2 - 1;i++){
+//            if(i % 2 == 0){
+//                for(int j = 0;j < xSize - 1;j++){
+//                    System.out.print("O");
+//
+//                    if(blockedX[i/2][j]){
+//                        System.out.print("--X--");
+//                    } else {
+//                        System.out.print("-----");
+//                    }
+//                }
+//                System.out.println("O");
+//            } else {
+//                for(int k = 0;k < 3;k++) {
+//                    for (int j = 0; j < xSize - 1; j++) {
+//                        if(k == 1) {
+//                            if(blockedY[i/2][j]) {
+//                                System.out.print("X     ");
+//                            } else {
+//                                System.out.print("|     ");
+//                            }
+//                        } else {
+//                            System.out.print("|     ");
+//                        }
+//                    }
+//                    System.out.println("|");
+//                }
+//            }
+//        }
 
         return "";
     }
--- src/Solver.java
+++ src/Solver.java
@@ -1,14 +1,41 @@
 /**
  * Created by S199756066 on 3/22/2017.
  */
-public class Solver {
+public class Solver extends Maze {
 
-    public Solver(){
+    int x = 0;
+    int y = 0;
+    boolean solved = false;
+    char heading = 'E';
 
+    public Solver(Maze maze){
+        super(maze.xSize, maze.ySize, maze.blockedX, maze.blockedY);
     }
 
     public void run(){
 
+    }
+
+    public void loop(){
+        while(!solved){
+
+        }
+    }
+
+    private void move(){
+        if(heading == 'E'){
+            x++;
+        } else if(heading == 'S'){
+            y++;
+        } else if(heading == 'W'){
+            x--;
+        } else if(heading == 'N'){
+            y--;
+        }
+    }
+
+    public void atIntersection(){
+        
     }
 
 }
